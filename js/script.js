1f4d67e05c4cbb75d2624704fe6bf0e35608c762
Judith <chinonsojudith618@gmail.com>
2022-07-23 17:17:42 +0100

    art-work

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
 
+const loginBtn = document.getElementById('login')
+const signUpBtn = document.getElementById('signup')
+const loginModal = document.getElementById('loginModal')
+const signUpModal = document.getElementById('signupModal')
+const closeLoginModal = document.getElementById('closeModal')
+const closeSignUpModal = document.getElementById('closeSignUpModal')
+
 btn.addEventListener('click', () =>{
     btn.classList.toggle('open')
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
+})
+
+// Open login modal
+loginBtn.addEventListener('click', ()=> {
+    loginModal.classList.add('block')
+    loginModal.classList.remove('hidden')
+})
+
+// Open signup modal
+signUpBtn.addEventListener('click', ()=> {
+    signUpModal.classList.add('block')
+    signUpModal.classList.remove('hidden')
+})
+
+// modal close button 
+closeLoginModal.addEventListener("click", ()=> {
+    loginModal.classList.add('hidden')
+})
+
+// modal close signup modal button 
+closeSignUpModal.addEventListener("click", ()=> {
+    signUpModal.classList.add('hidden')
 })
